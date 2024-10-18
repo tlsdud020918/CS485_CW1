666d8c2f721b6d96c68086ec9109f1e4b9a7368a
ShinyoungYu <sahaa0918@kaist.ac.kr>
2024-10-18 21:23:27 +0900

    first_commit_1

---
+++ CW1/question2/incremental_pca.py
@@ -0,0 +1,5 @@
+import scipy.io as sio
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.image as pmimage
---
+++ CW1/sample_code/RF_code/internal/getData.m
@@ -0,0 +1,78 @@
+function [ data_train, data_query ] = getData( MODE )
+% Generate training and testing data
+
+% Data Options:
+%   1. Toy_Gaussian
+%   2. Toy_Spiral
+%   3. Toy_Circle
+
+switch MODE
+    case 'Toy_Gaussian' % Gaussian distributed 2D points
+        %rand('state', 0);
+        %randn('state', 0);
+        N= 150;
+        D= 2;
+        
+        cov1 = randi(4);
+        cov2 = randi(4);
+        cov3 = randi(4);
+        
+        X1 = mgd(N, D, [randi(4)-1 randi(4)-1], [cov1 0;0 cov1]);
+        X2 = mgd(N, D, [randi(4)-1 randi(4)-1], [cov2 0;0 cov2]);
+        X3 = mgd(N, D, [randi(4)-1 randi(4)-1], [cov3 0;0 cov3]);
+        
+        X= real([X1; X2; X3]);
+        X= bsxfun(@rdivide, bsxfun(@minus, X, mean(X)), var(X));
+        Y= [ones(N, 1); ones(N, 1)*2; ones(N, 1)*3];
+        
+        data_train = [X Y];
+        
+    case 'Toy_Spiral' % Spiral (from Karpathy's matlab toolbox)
+        
+        N= 50;
+        t = linspace(0.5, 2*pi, N);
+        x = t.*cos(t);
+        y = t.*sin(t);
+        
+        t = linspace(0.5, 2*pi, N);
+        x2 = t.*cos(t+2);
+        y2 = t.*sin(t+2);
+        
+        t = linspace(0.5, 2*pi, N);
+        x3 = t.*cos(t+4);
+        y3 = t.*sin(t+4);
+        
+        X= [[x' y']; [x2' y2']; [x3' y3']];
+        X= bsxfun(@rdivide, bsxfun(@minus, X, mean(X)), var(X));
+        Y= [ones(N, 1); ones(N, 1)*2; ones(N, 1)*3];
+        
+        data_train = [X Y];
+        
+    case 'Toy_Circle' % Circle
+        
+        N= 50;
+        t = linspace(0, 2*pi, N);
+        r = 0.4;
+        x = r*cos(t);
+        y = r*sin(t);
+        
+        r = 0.8;
+        t = linspace(0, 2*pi, N);
+        x2 = r*cos(t);
+        y2 = r*sin(t);
+        
+        r = 1.2;
+        t = linspace(0, 2*pi, N);
+        x3 = r*cos(t);
+        y3 = r*sin(t);
+        
+        X= [[x' y']; [x2' y2']; [x3' y3']];
+        Y= [ones(N, 1); ones(N, 1)*2; ones(N, 1)*3];
+        
+        data_train = [X Y];
+end
+xrange = [-1.5 1.5];
+yrange = [-1.5 1.5];
+inc = 0.02;
+[x, y] = meshgrid(xrange(1):inc:xrange(2), yrange(1):inc:yrange(2));
+data_query = [x(:) y(:) zeros(length(x)^2,1)];
