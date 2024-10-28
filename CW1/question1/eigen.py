cdfd3a93411022ae96fd2eadbdde71acfd602f87
ShinyoungYu <sahaa0918@kaist.ac.kr>
2024-10-28 22:33:20 +0900

    Q1-experiment add

--- CW1/question1/eigen.py
+++ CW1/question1/eigen.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # set absolute path for importing split_data
 
-from dataset.data import split_data
+from dataset.data_static import split_data
 import numpy as np
 import matplotlib.pyplot as plt
 import time
@@ -21,7 +21,7 @@ def pca(A, mean_A, low=True):
     A = A - mean_A
 
     if low==True: 
-        S = (A.T@A)/total_num
+        S = (A.T@A) / total_num
         val, vec = np.linalg.eig(S)
         vec = A@vec
         vec = vec / np.linalg.norm(vec, axis=0)
@@ -29,9 +29,12 @@ def pca(A, mean_A, low=True):
     else:
         S = (A@A.T)/total_num
         val, vec = np.linalg.eig(S)
+        # rounding error solution
+        val = val.real
+        vec = vec.real
 
     # sort eigenvalues in descent order
-    idx = val.argsort()[::-1]
+    idx = np.argsort(val)[::-1]
     val = val[idx]
     vec = vec[:, idx]
 
@@ -46,6 +49,8 @@ def mean_face_reconstruction(mean_face):
     plt.imshow(reconstructed_mean_face, cmap='gray')
     plt.title('Mean Face')
     plt.show()
+    
+    return reconstructed_mean_face
 
 def plot_eig_val(val):
     A = val[val > 1]
@@ -58,26 +63,30 @@ def plot_eig_val(val):
     plt.title('Eigen Values')
     plt.show()
 
-def face_reconstruction(data, M=50):
-    eig_val, eig_vec = pca(train_data, mean_face)
-
+def face_reconstruction(data, eig_val, eig_vec, mean_face, M=50, isshow=False):
     centered_data = data - mean_face
 
-    projected_data = centered_data.T@eig_vec[:, :M] # project data onto PCA vector space with M=30
-    reconstructed_data = mean_face + eig_vec[:, :M]@projected_data.T
+    # eig_vec = D * D' / data = D * N
+    projected_data = eig_vec[:, :M].T @ centered_data # project data onto PCA vector space with M=30
+    reconstructed_data = mean_face + eig_vec[:, :M] @ projected_data
 
     theoretical_error = np.sum(eig_val[M:])
-    reconstruction_error = np.sum((data - reconstructed_data)**2, axis=0).mean()
+    reconstruction_error = np.sum((data - reconstructed_data)**2, axis=0)
+    reconstruction_error = np.mean(reconstruction_error)
 
-    # show only the first image of test dataset
-    recon_fig, (ax1, ax2) = plt.subplots(1, 2)
+    if isshow:
+        # show only the first image of test dataset
+        recon_fig, (ax1, ax2) = plt.subplots(1, 2)
 
-    ax1.imshow(data[:, 0].reshape((46, -1)).T, cmap='gray')
-    ax2.imshow(reconstructed_data[:, 0].reshape((46, -1)).T, cmap='gray')
-    ax1.set_title('Original')
-    ax2.set_title('Reconstructed with {} bases'.format(M))
+        ax1.imshow(data[:, 0].reshape((46, -1)).T, cmap='gray')
+        ax1.set_title('Original')
+        ax1.axis('off')
 
-    plt.show()
+        ax2.imshow(reconstructed_data[:, 0].reshape((46, -1)).T, cmap='gray')
+        ax2.set_title('Reconstructed with {} bases'.format(M))
+        ax2.axis('off')
+
+        plt.show()
 
     return theoretical_error, reconstruction_error # same when using train dataset
 
@@ -105,8 +114,8 @@ def knn_classifier(train_data, train_label, test_data, k=5, M=100):
     test_pred = np.array(test_pred)
 
     end = time.time()
-    print(f"{end - start: .5f} sec")
-    memory_usage(message='knn-classifier')
+    #print(f"{end - start: .5f} sec")
+    #memory_usage(message='knn-classifier')
     
     return test_pred
 
@@ -124,7 +133,7 @@ if __name__ == "__main__":
     # plot_eig_val(val[val > 1])
 
     # # reconstruct data based on pca using train data
-    # te, re = face_reconstruction(test_data)
+    # te, re = face_reconstruction(test_data, val2, vec2, mean_face)
     # print(te, re)
 
     # # knn classifier
